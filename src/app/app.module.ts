8fbdc835bacef494307cfa635412150369c1063d
trel01 <105283868+trel01@users.noreply.github.com>
2024-05-27 13:42:18 +0700

    Add Bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TestComponent } from './test/test.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    HomeComponent,
+    HeaderComponent,
+  
   ],
   imports: [
     BrowserModule,
