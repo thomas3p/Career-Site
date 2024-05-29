d6dd83ad3dd740f4b0a094ef3b1a588aecf6aae1
kongphop3212 <96216480+kongphop3212@users.noreply.github.com>
2024-05-29 10:54:46 +0700

    Team

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TeamsComponent } from './teams/teams.component';
+import { TeamDetailComponent } from './team-detail/team-detail.component';
+import { LocationsComponent } from './locations/locations.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/teams',
+    pathMatch: 'full'
+  },
+  {
+    path: 'teams',
+    component: TeamsComponent,
+  },
+  {
+    path: 'teams-detail/:title',
+    component: TeamDetailComponent,
+  },
+  {
+    path: 'locations',
+    component: LocationsComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
