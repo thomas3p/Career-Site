3d0c78ec5b17c1ba4ba62987c68b761691d5e926
thomas3p <137262816+thomas3p@users.noreply.github.com>
2024-05-29 09:08:34 +0700

    Mao jaddd

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -9,7 +9,7 @@ import { Component } from '@angular/core';
 export class AdminComponent {
   username: string = '';
   password: string = '';
-
+  loginstatus=false;
   constructor(private http: HttpClient) { }
 body={}
   login() {
@@ -21,11 +21,29 @@ body={}
     this.http.post('http://localhost:3000/login', this.body)
       .subscribe(
         response => {
-          console.log(response);
+          this.loginstatus = true
         },
         error => {
           console.log('Login failed:', error);
           // Handle login error (display error message, clear form fields, etc.)
+        }
+      );
+  }
+  title: string = '';
+  qualifications: string = '';
+
+
+  createJob(): void {
+    const jobData = { title: this.title, qualifications: this.qualifications /* Add other job data */ };
+    this.http.post<any>('http://localhost:3000/jobs', jobData)
+      .subscribe(
+        response => {
+          // Handle successful job creation
+          console.log('Job created successfully:', response);
+        },
+        error => {
+          // Handle job creation error
+          console.error('Error creating job:', error);
         }
       );
   }
