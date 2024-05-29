b4844d8c17e63e90f7ae51d31e4fba3c21420c4b
kongphop3212 <96216480+kongphop3212@users.noreply.github.com>
2024-05-29 17:07:33 +0700

    Locations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TeamsComponent } from './teams/teams.component';
-import { NavbarComponent } from './navbar/navbar.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { TeamDetailComponent } from './team-detail/team-detail.component';
 import { LocationsComponent } from './locations/locations.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { TeamDetailComponent } from './team-detail/team-detail.component';
+import { TeamsComponent } from './teams/teams.component';
+
 
 @NgModule({
   declarations: [
