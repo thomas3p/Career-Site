4238d35012088f7bd16b93a6edc4a961a3f3f4ce
thomas3p <137262816+thomas3p@users.noreply.github.com>
2024-05-30 13:08:52 +0700

    Admin

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,8 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../Service/auth.service';
-import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { JobService } from '../Service/job.service';
 
 @Component({
   selector: 'app-admin',
@@ -14,19 +12,20 @@ export class AdminComponent implements OnInit{
   password: string = '';
   
   title:string = ''
-  // qualifications
-  // details
-  // responsibility
-  // teamId
-  // locationId
-  // typeId
+  qualifications = []
+  details = []
+  responsibility = []
+  teamId = 0 
+  locationId = 0
+  typeId = 0 
   
   loginstatus = false
-  // constructor() { }
+  constructor(private http: HttpClient,private auth:AuthService) { }
   
   ngOnInit(): void {
     this.loginstatus = this.auth.getstatus()
   }
+
 
   login(){
     this.auth.login(this.username,this.password)
@@ -34,7 +33,9 @@ export class AdminComponent implements OnInit{
     
   }
   createJob(): void {
-    const jobData = { title: this.title, qualifications: this.qualifications /* Add other job data */ };
+    const jobData = { title: this.title, qualifications: this.qualifications 
+      ,details:this.details,responsibility:this.responsibility,teamId:this.teamId
+      ,locationId:this.locationId,typeId:this.typeId };
     this.http.post<any>('http://localhost:3000/jobs', jobData)
       .subscribe(
         response => {
@@ -47,51 +48,5 @@ export class AdminComponent implements OnInit{
         }
       );
   }
-  jobForm: FormGroup;
-
-  constructor(private fb: FormBuilder, private jobService: JobService,private http: HttpClient,private auth:AuthService) {
-    this.jobForm = this.fb.group({
-      title: ['', Validators.required],
-      qualifications: this.fb.array([this.fb.control('')]),
-      details: this.fb.array([this.fb.control('')]),
-      responsibility: this.fb.array([this.fb.control('')]),
-      teamId: ['', Validators.required],
-      locationId: ['', Validators.required],
-      typeId: ['', Validators.required]
-    });
-  }
-
-  get_qualifications() {
-    return this.jobForm.get('qualifications') as FormArray;
-  }
-
-  get_details() {
-    return this.jobForm.get('details') as FormArray;
-  }
-
-  get_responsibility() {
-    return this.jobForm.get('responsibility') as FormArray;
-  }
-
-  addQualification() {
-    this.qualifications.push(this.fb.control(''));
-  }
-
-  addDetail() {
-    this.details.push(this.fb.control(''));
-  }
-
-  addResponsibility() {
-    this.responsibility.push(this.fb.control(''));
-  }
-
-  onSubmit() {
-    if (this.jobForm.valid) {
-      this.jobService.createJob(this.jobForm.value).subscribe(response => {
-        console.log('Job created successfully', response);
-      }, error => {
-        console.error('Error creating job', error);
-      });
-    }
-  }
+  
 }
